e287e00817699e74cde4ff5b291943ca9544a3f5
Anjali Bhagat <anjalibhagat66a@gmail.com>
2024-09-13 15:50:52 +0200

    added the lisecence and changes

---
+++ life/src/life/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about life."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Anjali bhagat"
+__email__ = "bhagatanjali66a@gmail.com"
+__version__ = "0.1.0"
