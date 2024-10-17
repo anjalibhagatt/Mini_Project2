b9472a34f3a676199cf70c55ea22fdd6c2c1f3dd
anjalibhagatt <anjalibhagat26a@gmail.com>
2024-10-18 00:03:16 +0200

    Delete life directory

--- life/src/life/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about life."""
-
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Anjali bhagat"
-__email__ = "bhagatanjali66a@gmail.com"
-__version__ = "0.1.0"
